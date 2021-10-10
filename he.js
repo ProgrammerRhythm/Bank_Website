c0a9b15598a10ce1549b6b1ae3033eb591b5650d
Rhythm Munshi <programmingwithrm@gmail.com>
2021-10-10 14:55:02 +0600

    New Uploaded

---
+++ he.js
@@ -0,0 +1,11 @@
+let num = 10;
+// for(let i = 1; num<=100; i++){
+//     const num2 = num+1;
+//     const num3 = num2%2;
+//     console.log(num3)
+//     if (num2) {
+        
+//     }
+// }
+let num2 = num%2;
+console.log(num2);
--- main.js
+++ main.js
@@ -3,33 +3,39 @@ const Btn = document.getElementById('login');
 Btn.addEventListener('click', function () {
     const email = document.getElementById('email').value;
     const mainEmail = email.length;
+    
     const password = document.getElementById('password').value;
     const mainPassword = password.length;
-    console.log(mainPassword)
     if (mainEmail==0 && mainPassword==0) {
-        alert('Enter Email Id and Password')
+       alert('Enter Your Email Id And Password'); 
     }
-    if(mainEmail==0){
-            alert('Enter Email Id');
+    else if(mainEmail==0){
+       alert('Enter Your Email Id'); 
     }
     else if(mainPassword==0){
-        alert('Enter Password')
+        alert('Enter Your Password');
     }
     else{
     const loginArea = document.getElementById('login-area');
     loginArea.style.display = "none";
     const transfarArea = document.getElementById('transfar_Area');
-    transfarArea.style.display=('block');   
-} 
+    transfarArea.style.display=('block');
+}   
 })
 
 // deposit 
 const depoIn = document.getElementById('depo');
 depoIn.addEventListener('click',function(){
     const Amount = document.getElementById('amount').value;
+    const lenagth = Amount.length;
+    if (lenagth==0) {
+        alert('Enter Value')
+    }
+    else{
     const number = parseFloat(Amount);
     updateAmount('depoAmount',number);
     document.getElementById('amount').value = "";
+}
 })
 
 function updateAmount(id,number){
@@ -47,10 +53,16 @@ function updateAmount(id,number){
 // withdraw 
 const OutIt = document.getElementById('out');
 OutIt.addEventListener('click',function(){
-    const amount = document.getElementById('witha').value;
-    const Amount = parseFloat(amount);
+    const cost = document.getElementById('witha').value;
+    const lenagth2 = cost.length;
+    if (lenagth2==0) {
+        alert('Enter Value')
+    }
+    else{
+    const Amount = parseFloat(cost);
     Withdraw('currentOut',Amount) 
     document.getElementById('witha').value = "";
+}
 })
 function Withdraw(id, num) {
     const OutAmount = document.getElementById(id).innerText;
